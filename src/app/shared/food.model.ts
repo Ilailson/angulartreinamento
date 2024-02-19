0b2f38fe0d512554e74f792db81e9ab018f7f6a3
Ilailson Rocha <ilailsonrocha@iec.gov.br>
2024-02-19 12:49:47 -0300

    Removido

--- src/app/shared/food.model.ts
+++
@@ -1,9 +0,0 @@
-export class Food {
-    // public name: string
-    // public amout: number
-
-    constructor(public name: string, public amout: number){
-        // this.name=name
-        // this.amout=amout
-    }
-}
